955d0487784c01b9cde1caf3a134318786c5cb85
Elijah-Sparrow <sparrowkids2008@gmail.com>
2026-09-09 14:38:18 -0500

    Dealer State will give cards based on Texas Hold-em rules

--- src/App.java
+++ src/App.java
@@ -2,9 +2,14 @@ public class App {
     public static void main(String[] args) throws Exception {
        // System.out.println("Hello, World!");
         Deck test = new Deck();
+        DealerState dealer = new DealerState();
         test.shuffle();
         Player me = new Player("Elijah", 0);
         me.deal(test);
+        dealer.comCards(test);
+        dealer.comCards(test);
+        dealer.comCards(test);
         System.out.println(me.toString());
+        System.out.println("\n" + dealer.toString());
     }
 }
--- src/DealerState.java
+++ src/DealerState.java
@@ -1,13 +1,30 @@
 public class DealerState {
     private final String dealer;
-    private Card[] communityCards;
+    private Card[] communityCards = new Card[5];
     public DealerState(){
         dealer = "dealer";
     }
-    public String comCards(Deck cards){
+    public void comCards(Deck cards){
         if (communityCards[0] == null){
-            
+            communityCards[0] = cards.take();
+            communityCards[1] = cards.take();
+            communityCards[2] = cards.take();
         }
-        return communityCards.toString();
+        else if (communityCards[3] == null){
+            communityCards[3] = cards.take();
+        }
+        else if (communityCards[4] == null){
+            communityCards[4] = cards.take();
+        }
+    }
+
+    public String toString(){
+        String s = "Dealer: " + dealer + "\nCommunity Cards: ";
+        for (int i = 0; i < communityCards.length; i++){
+            if (communityCards[i] != null){
+                s += communityCards[i].toString() + ", ";
+            }
+        }
+        return s;
     }
 }
